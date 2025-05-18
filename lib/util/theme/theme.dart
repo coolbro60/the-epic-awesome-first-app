d166335424f116ec6aca5636d9a158461cd597b5
Crystal Zhu <crystal.zhu.tt@hotmail.com>
2025-05-17 23:22:33 -0700

    light/dark text theme

--- lib/util/theme/theme.dart
+++ lib/util/theme/theme.dart
@@ -1,4 +1,6 @@
-import "package:flutter/material.dart";
+import 'package:MediCheck/util/theme/custom_theme/elevated_button_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:MediCheck/util/theme/custom_theme/text_theme.dart';
 
 class TAppTheme {
   TAppTheme._();
@@ -6,10 +8,21 @@ class TAppTheme {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true, 
     //fontFamily:
-    // brightness: ligh
+    brightness: Brightness.light,
+    primaryColor: Colors.teal, 
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
   );
 
   static ThemeData darkTheme = ThemeData(
+    useMaterial3: true, 
+    //fontFamily:
+    brightness: Brightness.dark,
+    primaryColor: Colors.teal, 
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
 
   );
 }
