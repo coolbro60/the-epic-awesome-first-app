2fd36143dbf1802005d31768e4e2b9d8e2b88875
iboss2000 <69155297+coolbro60@users.noreply.github.com>
2025-05-19 21:10:30 -0700

    withValues update
    withopacity is deprecated

--- lib/util/constants/colors.dart
+++ lib/util/constants/colors.dart
@@ -33,7 +33,7 @@ class TColors {
 
   //background container colors
   static const Color lightContainer = Color(0xFFF6F6F6);
-  static Color darkContainer = TColors.textWhite.withOpacity(0.1);
+  static Color darkContainer = TColors.textWhite.withValues(alpha: 0.1);
 
   //Button Colors
   static const Color buttonPrimary = primary;
