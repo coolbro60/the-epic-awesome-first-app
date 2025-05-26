4222c32a76c3c142f1e8ebba2554712ad8e90e17
iboss2000 <69155297+coolbro60@users.noreply.github.com>
2025-05-25 20:53:24 -0700

    first part of onboarding

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:MediCheck/features/authentication/screens/onboarding.dart';
+import 'package:MediCheck/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:MediCheck/util/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
